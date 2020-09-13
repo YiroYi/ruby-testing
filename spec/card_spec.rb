f822fc2d1b1f95c16179a21ccaf45983ecc71cc3
YiroYi <yiroyi@live.com>
2020-09-12 20:04:18 -0500

    Card lesson complete

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -22,6 +22,9 @@ RSpec.describe Card do #NAme of class, open the test or can pass Class name
   # end
 
   # let (symbol) this convert the symbol into a variable
+  # also let is a kind of lazy loading because it is not going to
+  # be loaded until it is called inside the example block
+
   let(:card) { Card.new('Ace','Spades') }
 
   it 'has a rank and that rank can change' do
@@ -32,5 +35,12 @@ RSpec.describe Card do #NAme of class, open the test or can pass Class name
 
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom message error' do
+    comparision = 'Spades'
+    expect(card.suit).to eq(comparision), "Hey I expected #{comparision}, but i got #{card.suit}"
+    # after eq it can be added a second paramater that it
+    # is a custom message
   end
 end
