9242b6642368874ba7fde4076bcac8ddffee1585
YiroYi <yiroyi@live.com>
2020-09-12 19:51:39 -0500

    using -let- keyword

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,17 +1,36 @@
 class Card
-  attr_reader :type
+  attr_accessor :rank, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
 
 
-RSpec.describe 'Card' do #NAme of class, open the test
+RSpec.describe Card do #NAme of class, open the test or can pass Class name
 # it its to name the test
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+
+  # "before" is going to run before each example case.
+  # before do
+  #   @card = Card.new('Ace','Spades')
+  # end
+
+  # def card
+  #   Card.new('Ace','Spades')
+  # end
+
+  # let (symbol) this convert the symbol into a variable
+  let(:card) { Card.new('Ace','Spades') }
+
+  it 'has a rank and that rank can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
   end
 end
